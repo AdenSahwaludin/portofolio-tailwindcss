657ebce68f0e55da78af3b890691f06ca8550772
AdenSahwaludin <sekha1211@gmail.com>
2023-10-11 09:53:06 +0700

    Add skills

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,18 +17,19 @@ module.exports = {
 			},
 			keyframes: {
 				wavingHand: {
-					"0%": { transform: "rotate(0deg)" },
+					"0%": { transform: "rotate(0deg) " },
 					"10%": { transform: "rotate(14deg)" },
 					"20%": { transform: "rotate(-8deg)" },
 					"30%": { transform: "rotate(14deg)" },
 					"40%": { transform: "rotate(-4deg)" },
-					"50%": { transform: "rotate(10.0deg)" },
-					"60%": { transform: "rotate(0.0deg)" },
-					"100%": { transform: "rotate(0.0deg)" },
+					"50%": { transform: "rotate(10deg)" },
+					"60%": { transform: "rotate(0deg)" },
+					"100%": { transform: "rotate(0deg)" },
 				},
 			},
 			animation: {
 				wavingHand: "wavingHand 2s linear infinite",
+				"bounce-slow": "bounce 1.2s ease-out infinite",
 			},
 		},
 	},
