960aecd95b3e7a8232105b42b5d439adc3dbd42d
AdenSahwaludin <sekha1211@gmail.com>
2023-10-14 07:54:59 +0700

    Little modification

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
+	content: {
+		relative: true,
+		transform: (content) => content.replace(/taos:/g, ""),
+		files: ["index.html"],
+	},
 	content: ["index.html"],
+	safelist: [
+		"!duration-[0ms]",
+		"!delay-[0ms]",
+		'html.js :where([class*="taos:"]:not(.taos-init))',
+	],
 	darkMode: "class",
 	theme: {
 		container: {
@@ -42,5 +52,5 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [],
 };
